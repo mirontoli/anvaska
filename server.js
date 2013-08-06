4562b24e4441a34bace89c0806ce1f577f0db13c
Anatoly Mironov <mirontoli@gmail.com>
2013-08-06 17:37:16 -0400

    Remove dependencies

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.delete('/api/records/:id', function (req, res){
      res.send(result);
   });  
 })
-
+*/
 app.listen(process.env.PORT || "8080");
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-*/
+
