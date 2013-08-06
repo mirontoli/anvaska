faaa005a35b8dcdbd4f647c7a5648f401e2b1589
Anatoly Mironov <mirontoli@gmail.com>
2013-08-06 17:49:42 -0400

    express simple

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
+/*
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(process.env.PORT || "8080");
-
+*/
 
 
 var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.send('hello world express');
 });
+
+app.listen(process.env.PORT || "8080");
+
 
 /*
 var application_root = __dirname,
