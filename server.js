996b3904a38d64c358cc775d0941bd36c4ee0bb7
Anatoly Mironov <mirontoli@gmail.com>
2013-08-06 18:22:55 -0400

    add routing

--- server.js
+++ server.js
@@ -29,8 +29,34 @@ app.post('/', function(req, res){
     res.redirect('/');
 });
 
-app.listen(process.env.PORT || "8080");
+
 var recordProvider = new RecordProvider(process.env.MONGOLAB_URI);
+
+
+
+//Routes
+app.get('/api', function (req, res) {
+  res.send('anvaska API is running');
+});
+app.get('/api/records', function (req, res){
+  recordProvider.findAll(function(error, data) {
+     res.send(data);
+  });
+});
+
+app.post('/api/records', function (req, res){
+  var records = req.body.records;
+  recordProvider.save(records, function(error, data) {
+     res.send(data);
+  });
+});
+app.delete('/api/records/:id', function (req, res){
+  console.log(req.params.id);
+  recordProvider.deleteById(req.params.id, function(error, result) {
+     res.send(result);
+  });  
+});
+app.listen(process.env.PORT || "8080");
 /*
 
 
@@ -43,34 +69,7 @@ app.configure(function() {
   //app.use(express.static(path.join(application_root, "public")));
 });
 */
-
 /*
-//Routes
-app.get('/api', function (req, res) {
-  res.send('anvaska API is running');
-});
-app.get('/api/records', function (req, res){
-  recordProvider.findAll(function(error, data) {
-     res.send(data);
-  });
-});
-app.post('/', function(req, res) {
-    res.redirect("/blog");
-    
-});
-app.post('/api/records', function (req, res){
-  var records = req.body.records;
-  recordProvider.save(records, function(error, data) {
-     res.send(data);
-  });
-});
-app.delete('/api/records/:id', function (req, res){
-  console.log(req.params.id);
-  recordProvider.deleteById(req.params.id, function(error, result) {
-     res.send(result);
-  });  
-})
-
 app.listen(process.env.PORT || "8080");
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 */
