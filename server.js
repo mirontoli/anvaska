5657298c8e1c143a17b76e83b835c8348a1f868b
Anatoly Mironov <mirontoli@gmail.com>
2013-08-06 18:14:24 -0400

    Redirect POST to GET

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ var express = require("express")
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
+app.post('/', function(req, res){
+    res.redirect('/');
+});
 
 app.listen(process.env.PORT || "8080");
 /*
