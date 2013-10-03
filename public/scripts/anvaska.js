4dcd7ff4c3447d71e94b996fb60152205009c560
Anatoly Mironov <mirontoli@gmail.com>
2013-10-03 02:09:01 -0400

    remove chrome control

--- public/scripts/anvaska.js
+++ public/scripts/anvaska.js
@@ -223,6 +223,7 @@ anvaska.ui.initDate = function() {
    $("#anvaska-date-input").datepicker({ dateFormat: 'yy-mm-dd' });
 };
 anvaska.ui.init = function() {
+    return;
    anvaska.ui.initDate();
 };
 $(document).on({
